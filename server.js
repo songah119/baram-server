378fa47705b95c0823e524d4ea6928548a5dbc5c
songah119 <skutcm119@gmail.com>
2022-03-07 23:23:05 +0900

    중간 commit

--- server.js
+++ server.js
@@ -7,11 +7,38 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/products",(req,res) =>{
-    res.send("업로드 된 상품 입니다.");
+    res.send({
+        products : [
+            {
+                id:1,
+                name: "유기농 순면커버 생리대",
+                maker: "라엘",
+                price: 4290,
+                imageUrl: "./images/products/product1.jpg",
+            },
+            {
+                id:2,
+                name: "러브 오버나이트 (중형)",
+                maker: "시크릿데이",
+                price: 3500,
+                imageUrl: "./images/products/product2.jpg",
+            },
+            {
+                id:3,
+                name: "쏘피 한결 생리대",
+                maker: "쏘피",
+                price: 7500,
+                imageUrl: "./images/products/product3.jpg",
+            },
+        ],
+    });
 });
 
 app.post("/products",(req, res)=>{
-    res.send("상품이 등록되었습니다.");
+    const body= req.body;
+    res.send({
+        body: body,
+    },);
 });
 
 app.listen(port, ()=> {

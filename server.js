010a5eb9233d0c350cd1d00c16f7b836e1955e72
nayawgj <nayawgj@gmail.com>
2022-03-07 23:21:17 +0900

    상품 정보 반환

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
-const express =require('express');
-const cors= require('cors');
-const app=express();
-const port= 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/products",(req,res) =>{
-    res.send("업로드 된 상품 입니다.");
+app.get("/products", (req, res) => {
+  res.send({
+    products: [
+      {
+        name: "유기농 순면커버 생리대",
+        maker: "라엘",
+        price: 4290,
+        imageUrl: "./images/products/product1.jpg",
+        ranking: 1,
+      },
+      {
+        name: "러브 오버나이트 (중형)",
+        maker: "시크릿 데이",
+        price: 3500,
+        imageUrl: "./images/products/product2.jpg",
+        ranking: 2,
+      },
+      {
+        name: "쏘피 한결 생리대",
+        maker: "쏘피",
+        price: 7500,
+        imageUrl: "./images/products/product3.jpg",
+        ranking: 3,
+      },
+    ],
+  });
 });
 
-app.post("/products",(req, res)=>{
-    res.send("상품이 등록되었습니다.");
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  res.send(`id가 ${id}인 상품의 상세정보를 받아오는 중입니다.`);
 });
 
-app.listen(port, ()=> {
-    console.log("바람웹서버 동작");
+app.listen(port, () => {
+  console.log("바람 웹서버 동작");
 });
